70f756a2084ec179836ba5ec9005726d146759fc
JuanWalden <hdementes@gmail.com>
2025-07-25 11:29:39 +0200

    feat: Configuración de base path para despliegue y actualización de build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ¡Añade esta línea!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
