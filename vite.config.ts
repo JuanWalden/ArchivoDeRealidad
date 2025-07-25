adf115e7dcafba7df551b94dda84fe6c585bb26d
JuanWalden <hdementes@gmail.com>
2025-07-25 11:59:15 +0200

    feat: Construcción en carpeta docs para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // ¡Añade esta línea!
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: { // Añade este bloque
+    outDir: 'docs', // Esto cambiará la carpeta de salida de 'dist' a 'docs'
   },
 });
